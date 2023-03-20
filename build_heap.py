b59cfca7556a2b766996781e44638acd6de8b595
viktorija-j <120105424+viktorija-j@users.noreply.github.com>
2023-03-20 09:29:17 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,50 @@
 #211RDB250
 
-def heapify(i, data):
-    parent = (i - 1) // 2
+def heapify(data, n, i, swaps):
+    smallest = i
+    left_child = 2 * i + 1
+    right_child = 2 * i + 2
 
-    if i == 2 and len(data) == 3:
-        parent = 0
+    if left_child < n and data[left_child] < data[smallest]:
+        smallest = left_child
 
-    if parent < 0:
-        return 0
+    if right_child < n and data[right_child] < data[smallest]:
+        smallest = right_child
 
-    if data[i] < data[parent]:
-        print(parent, i)
-        data[i], data[parent] = data[parent], data[i]
-        return 1 + heapify(parent, data)
-
-    return 0
+    if smallest != i:
+        swaps.append((i, smallest))
+        data[i], data[smallest] = data[smallest], data[i]
+        heapify(data, n, smallest, swaps)
 
 
 def build_heap(data):
-    n = len(data)
     swaps = []
+    n = len(data)
 
-    for i in range(n // 2, -1, -1):
-        for j in range(heapify(i, data)):
-            swaps.append((i, (i - 1) // 2))
-
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(data, n, i, swaps)
     return swaps
 
 
 def main():
-    n = int(input())
-    data = list(map(int, input().split()))
+
+    input_type = input("I or F: ")
+    if input_type == "I":
+        n = int(input())
+        data = list(map(int, input().split()))
+
+
+    elif input_type == "F":
+        filename = input("File name: ")
+        with open("tests/"+filename, 'r') as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
 
     assert len(data) == n
-
     swaps = build_heap(data)
-
+    print("")
     print(len(swaps))
+
     for i, j in swaps:
         print(i, j)
 

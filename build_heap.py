8444703a24ebab1cde0f4810e8b97f9b4a82320d
viktorija-j <120105424+viktorija-j@users.noreply.github.com>
2023-03-20 09:09:08 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,40 +1,28 @@
 #211RDB250
 
-def biuld_heap(i, data):
-    parent = (i//2) - 1
-    if i == 3:
-        parent = 1
+def sift_down(i, n, data, swaps):
+    min_index = i
+    left_child = 2 * i + 1
+    if left_child < n and data[left_child] < data[min_index]:
+        min_index = left_child
+    right_child = 2 * i + 2
+    if right_child < n and data[right_child] < data[min_index]:
+        min_index = right_child
+    if i != min_index:
+        data[i], data[min_index] = data[min_index], data[i]
+        swaps.append((i, min_index))
+        sift_down(min_index, n, data, swaps)
 
-    if parent < 0:
-        return
-
-    if data[i] < data[parent]:
-        # swap parent and child
-        print(parent, i)
-        data[i], data[parent] = data[parent], data[i]
-        biuld_heap(parent, data)
-
-def main():
-    inp = input()
-    if inp == "I":
-        n = int(input())
-        data = list(map(int, input().split()))
-    elif inp == "F":
-        print("F")
-        return
-
-    assert len(data) == n
-
-    leaves = list(range(n//2, n))
-    print("leaves =", leaves, "\n")
-
-    for k in leaves[::-1]:
-        biuld_heap(k, data)
-
-    # output how many swaps were made
-    # this number should be less than 4n (less than 4*len(data))
-    print(1)
+def build_heap(n, data):
+    swaps = []
+    for i in range(n // 2, -1, -1):
+        sift_down(i, n, data, swaps)
+    return swaps
 
 if __name__ == "__main__":
-    main()
-
+    n = int(input())
+    data = list(map(int, input().split()))
+    swaps = build_heap(n, data)
+    print(len(swaps))
+    for swap in swaps:
+        print(swap[0], swap[1])
